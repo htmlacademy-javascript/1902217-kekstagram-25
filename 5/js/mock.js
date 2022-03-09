2e1bedd4a930a25c15cd97e8820d4803146bc253
Keks <keks@htmlacademy.ru>
2022-03-09 17:10:25 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/mock.js
@@ -0,0 +1,42 @@
+import {getRandomNumber, getRandomArrayElement} from './utils.js';
+
+const DESCRIPTION = 'Это какое то описание';
+
+const LIST_OF_COMMENTATORS = [
+  'Василий Алибабаевич',
+  'Чунгачанга',
+  'Кузьма',
+  'Уинстон Черчиль',
+  'Иванов Иван Иванович',
+  'Алеша Попович',
+  'Мистер Комментарий',
+  'Диванный аналитик',
+  'Эксперт по жизни',
+  'Шурик',
+];
+
+const LIST_OF_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const createObject = (item, index) => ({
+  id: index + 1,
+  url: `photos/${index + 1}.jpg`,
+  description: DESCRIPTION,
+  likes: getRandomNumber(15, 200),
+  comments: [{
+    id: index + 1,
+    avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+    message: getRandomArrayElement(LIST_OF_COMMENTS),
+    name: getRandomArrayElement(LIST_OF_COMMENTATORS)
+  }]
+});
+
+const getUsersPhotos = (count) => Array.from({length: count}, createObject);
+
+export {getUsersPhotos};
